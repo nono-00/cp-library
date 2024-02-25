0ddc7e2cb22bf8e8c2d35d3c887b2f8a427636c6
nono <107781334+nono-00@users.noreply.github.com>
2024-02-25 21:14:40 +0900

    add articulation points (maybe has bug)

--- src/graph/articulation-points.hpp
+++ src/graph/articulation-points.hpp
@@ -1,65 +1,51 @@
 #pragma once
 
 #include <algorithm>
-#include <limits>
+#include <cassert>
 #include <vector>
 
-#include "graph/internal-graph-concepts.hpp"
+#include "graph/base.hpp"
 
 namespace nono {
 
-//  brief:
-//  - 関節点列挙
-//
-//  complexity:
-//  - O(V + E)
-//
-//  param:
-//  - `graph`: 無向グラフ. 連結であるかどうかは問わない.
-//
-//  note:
-//  - 非単純グラフでも動く（はず）
-template <internal::IndexedGraph GraphType>
-std::vector<int> articulation_points(const GraphType& graph) {
+template <class T>
+std::vector<int> articulation_points(const Graph<T>& graph) {
+    assert(graph.is_undirected());
     constexpr int NONE = -1;
-
     int n = graph.size();
     std::vector<int> order(n, NONE);
-    std::vector<int> low(n);
-    std::vector<int> result;
-    int now = 0;
+    std::vector<int> lowlink(n, NONE);
+    std::vector<int> parent(n, NONE);
+    std::vector<int> children(n);
 
+    int now = 0;
     auto dfs = [&](const auto& dfs, int u, int edge_id) -> void {
-        bool add = false;
-        order[u] = now++;
-        low[u] = order[u];
-        int child = 0;
-        for (const auto& edge: graph[u]) {
-            if (edge.id == edge_id) continue;
-            if (order[edge.to] == NONE) {
-                child++;
-                dfs(dfs, edge.to, edge.id);
-                low[u] = std::min(low[u], low[edge.to]);
-                if (edge_id != NONE and order[u] <= low[edge.to] and not add) {
-                    result.push_back(u);
-                    add = true;
-                }
+        order[u] = lowlink[u] = now++;
+        for (const auto& e: graph[u]) {
+            if (e.id == edge_id) continue;
+            if (order[e.to] == NONE) {
+                parent[e.to] = u;
+                children[u]++;
+                dfs(dfs, e.to, e.id);
+                lowlink[u] = std::min(lowlink[u], lowlink[e.to]);
             } else {
-                low[u] = std::min(low[u], order[edge.to]);
+                lowlink[u] = std::min(lowlink[u], order[e.to]);
             }
         }
-
-        if (edge_id == NONE && child >= 2) {
-            result.push_back(u);
-        }
     };
-
     for (int i = 0; i < n; i++) {
         if (order[i] == NONE) {
             dfs(dfs, i, NONE);
         }
     }
-
+    std::vector<int> result;
+    for (int i = 0; i < n; i++) {
+        if (parent[i] == NONE) {
+            if (children[i] >= 2) result.push_back(i);
+        } else if (lowlink[i] >= order[i]) {
+            result.push_back(i);
+        }
+    }
     return result;
 }
 
