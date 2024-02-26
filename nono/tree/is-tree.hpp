955827509ee6a86249d10eb5f53082452fe77f9f
nono <107781334+nono-00@users.noreply.github.com>
2024-02-26 10:41:26 +0900

    fix is-tree

--- nono/tree/is-tree.hpp
+++ nono/tree/is-tree.hpp
@@ -3,7 +3,7 @@
 #include <algorithm>
 #include <vector>
 
-#include "nono/tree/internal-tree-concepts.hpp"
+#include "nono/graph/base.hpp"
 
 namespace nono {
 
@@ -11,22 +11,23 @@ namespace tree {
 
 //  brief:
 //  - 木かどうか判定する
-template <internal::Tree TreeType>
-bool is_tree(const TreeType& graph) {
+template <class T>
+bool is_tree(const Graph<T>& graph) {
+    if (graph.is_directed()) return false;
     constexpr int NONE = -1;
     int n = graph.size();
-    std::vector<bool> used(n);
-    auto dfs = [&](auto self, int u, int p) -> bool {
-        used[u] = true;
+    std::vector<int> used(n);
+    auto dfs = [&](auto self, int u, int eid) -> bool {
+        used[u] = 1;
         for (const auto& e: graph[u]) {
-            if (e.to == p) continue;
+            if (e.id == eid) continue;
             if (used[e.to]) return false;
             if (!self(self, e.to, u)) return false;
         }
         return true;
     };
     if (!dfs(dfs, graph.root(), NONE)) return false;
-    return std::ranges::all_of(used, [](bool f) { return f; });
+    return std::ranges::all_of(used, [](int f) { return f == 1; });
 }
 
 }  //  namespace tree
