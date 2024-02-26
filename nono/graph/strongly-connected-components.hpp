856f0aed8d70d9a6099563781ebd9f18c02488fa
nono <107781334+nono-00@users.noreply.github.com>
2024-02-26 09:03:35 +0900

    fix scc

--- nono/graph/strongly-connected-components.hpp
+++ nono/graph/strongly-connected-components.hpp
@@ -3,7 +3,7 @@
 #include <algorithm>
 #include <vector>
 
-#include "nono/graph/internal-graph-concepts.hpp"
+#include "nono/graph/base.hpp"
 
 namespace nono {
 
@@ -21,49 +21,44 @@ namespace nono {
 //
 //  note:
 //  - Tarjan's algorithmを使用している.
-//  -
-//  - Kosaraju's algorithmも実装した.
-//  - コメントアウトしてあるが使える（はず）
-template <internal::Graph GraphType>
-std::vector<std::vector<int>> strongly_connected_components(const GraphType& graph) {
+template <class T>
+std::vector<std::vector<int>> strongly_connected_components(const Graph<T>& graph) {
     constexpr int NONE = -1;
 
     int n = graph.size();
     std::vector<int> order(n, NONE);
     std::vector<int> group_ids(n, NONE);
     std::vector<int> count(n);
-    std::vector<int> stack;
-    stack.reserve(n);
+    std::vector<int> history;
+    history.reserve(n);
 
     int now = 0;
     int group_id = 0;
 
     auto dfs = [&](const auto& self, int u) -> int {
-        int low = now;
-        order[u] = now;
-        now++;
-        stack.push_back(u);
+        int lowlink = order[u] = now++;
+        history.push_back(u);
 
         for (const auto& edge: graph[u]) {
             if (order[edge.to] == NONE) {
-                low = std::min(low, self(self, edge.to));
+                lowlink = std::min(lowlink, self(self, edge.to));
             } else if (group_ids[edge.to] == NONE) {
-                low = std::min(low, order[edge.to]);
+                lowlink = std::min(lowlink, order[edge.to]);
             }
         }
 
-        if (low == order[u]) {
-            while (!stack.empty()) {
-                int v = stack.back();
+        if (lowlink == order[u]) {
+            while (!history.empty()) {
+                int v = history.back();
                 if (order[v] < order[u]) break;
-                stack.pop_back();
+                history.pop_back();
                 group_ids[v] = group_id;
                 count[group_id]++;
             }
             group_id++;
         }
 
-        return low;
+        return lowlink;
     };
 
     for (int i = 0; i < n; i++) {
@@ -74,82 +69,12 @@ std::vector<std::vector<int>> strongly_connected_components(const GraphType& gra
 
     std::vector<std::vector<int>> groups(group_id);
     for (int i = 0; i < group_id; i++) {
-        groups[i].reserve(count[i]);
+        groups[group_id - i - 1].reserve(count[i]);
     }
     for (int i = 0; i < n; i++) {
-        groups[group_ids[i]].push_back(i);
+        groups[group_id - group_ids[i] - 1].push_back(i);
     }
-    std::reverse(groups.begin(), groups.end());
     return groups;
 }
 
 }  //  namespace nono
-
-//  #pragma once
-//
-//  #include <algorithm>
-//  #include <utility>
-//  #include <vector>
-//
-//  namespace nono {
-//
-//  namespace internal::scc {
-//
-//  using SccGraph = std::vector<std::vector<int>>;
-//
-//  void dfs(const SccGraph& graph, int u, std::vector<int>& group, std::vector<bool>& discovered) {
-//      discovered[u] = true;
-//      for (int v: graph[u]) {
-//          if (not discovered[v]) {
-//              dfs(graph, v, group, discovered);
-//          }
-//      }
-//      group.push_back(u);
-//  }
-//
-//  template <class GraphType>
-//  std::pair<SccGraph, SccGraph> to_scc_graph(const GraphType& graph) {
-//      int n = graph.size();
-//      SccGraph lhs(n), rhs(n);
-//      for (int i = 0; i < n; i++) {
-//          for (const auto& edge: graph[i]) {
-//              lhs[edge.from].push_back(edge.to);
-//              rhs[edge.to].push_back(edge.from);
-//          }
-//      }
-//      return {lhs, rhs};
-//  }
-//
-//  }  //  namespace internal::scc
-//
-//  template <class GraphType>
-//  std::vector<std::vector<int>> strongly_connected_components(const GraphType& graph) {
-//      int n = graph.size();
-//      std::vector<int> finished;
-//      std::vector<bool> discovered(n);
-//      finished.reserve(n);
-//      const auto [scc_graph, inv_scc_graph] = internal::scc::to_scc_graph(graph);
-//
-//      //  各頂点の探索が終わるタイミングを記録
-//      for (int i = 0; i < n; i++) {
-//          if (not discovered[i]) {
-//              internal::scc::dfs(scc_graph, i, finished, discovered);
-//          }
-//      }
-//
-//      std::reverse(finished.begin(), finished.end());
-//      discovered.assign(n, false);
-//      std::vector<std::vector<int>> groups;
-//
-//      for (int i: finished) {
-//          if (not discovered[i]) {
-//              std::vector<int> group;
-//              internal::scc::dfs(inv_scc_graph, i, group, discovered);
-//              groups.emplace_back(group);
-//          }
-//      }
-//
-//      return groups;
-//  }
-//
-//  }  //  namespace nono
--- test/graph/strongly-connected-components/lc-scc.test.cpp
+++ test/graph/strongly-connected-components/lc-scc.test.cpp
@@ -2,8 +2,7 @@
 #include <iostream>
 #include <vector>
 
-#include "nono/graph/csr-graph.hpp"
-#include "nono/graph/edge.hpp"
+#include "nono/graph/base.hpp"
 #include "nono/graph/strongly-connected-components.hpp"
 
 namespace nono {
@@ -11,22 +10,22 @@ namespace nono {
 void solve() {
     int n, m;
     std::cin >> n >> m;
-    std::vector<Edge<int>> edges;
+    std::vector<Edge> edges;
     edges.reserve(m);
     for (int i = 0; i < m; i++) {
         int u, v;
         std::cin >> u >> v;
         edges.emplace_back(u, v);
     }
-    const CSRGraph graph(n, edges);
+    const auto graph = to_directed_graph(n, edges);
     auto groups = strongly_connected_components(graph);
     std::cout << groups.size() << '\n';
     for (const auto& group: groups) {
-        std::cout << group.size() << ' ';
-        for (int i = 0; int v: group) {
-            std::cout << v << (i + 1 < (int)group.size() ? ' ' : '\n');
-            i++;
+        std::cout << group.size();
+        for (int v: group) {
+            std::cout << ' ' << v;
         }
+        std::cout << '\n';
     }
 }
 
