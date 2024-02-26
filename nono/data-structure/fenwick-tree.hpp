a4304ba7330ac4f20994731acb1befe381bddad9
nono <107781334+nono-00@users.noreply.github.com>
2024-02-27 05:49:44 +0900

    fix fenwick-tree

--- nono/data-structure/fenwick-tree.hpp
+++ nono/data-structure/fenwick-tree.hpp
@@ -5,53 +5,46 @@
 
 namespace nono {
 
-//  brief:
-//  - FenwickTree. 区間和の取得が可能.
-//
-//  note:
-//  - `T` が数値型ならOK.
-//  - 数値型でなかったとしても
-//  - 1. ゼロ初期化によって単位元を得られる
-//  - 2. `+`, `-` が定義されている
-//  - ならOK.
-//  
-//  todo:
-//  - FenwickTree上の二分探索
-template <class T>
+template <class G>
 class FenwickTree {
-  public:
-    FenwickTree(int size = 0): size_(size), data_(size_ + 1) {}
+    using T = G::value_type;
 
-    //  brief:
-    //  - `i` 番目の要素に `v` を加算する
-    void add(int i, const T& v) {
-        assert(0 <= i && i < size_);
-        for (++i; i <= size_; i += i & -i) {
-            data_[i] += v;
+  public:
+    FenwickTree(int n): n_(n), data_(n_ + 1, G::e()) {}
+
+    void apply(int i, T elem) {
+        assert(0 <= i && i < n_);
+        for (i++; i <= n_; i += i & -i) {
+            data_[i] = G::op(data_[i], elem);
         }
     }
 
-    //  brief:
-    //  - `[0, i)` の和を取得する
-    T sum(int i) const {
-        assert(0 <= i && i <= size_);
-        T result{};
+    void set(int i, T elem) {
+        assert(0 <= i && i < n_);
+        apply(i, G::op(elem, G::inv(get(i))));
+    }
+
+    T prod(int i) const {
+        assert(0 <= i && i <= n_);
+        T result = G::e();
         for (; i > 0; i -= i & -i) {
-            result += data_[i];
+            result = G::op(result, data_[i]);
         }
         return result;
     }
 
-    //  brief:
-    //  - `[l, r)` の和を取得する
-    T sum(int l, int r) const {
-        assert(0 <= l && l <= size_);
-        assert(l <= r && r <= size_);
-        return sum(r) - sum(l);
+    T prod(int left, int right) const {
+        assert(left <= right);
+        return G::op(prod(right), G::inv(prod(left)));
+    }
+
+    T get(int i) const {
+        assert(0 <= i && i < n_);
+        return prod(i, i + 1);
     }
 
   private:
-    int size_;
+    int n_;
     std::vector<T> data_;
 };
 
--- test/data-structure/fenwick-tree/lc-static-range-sum.test.cpp
+++ test/data-structure/fenwick-tree/lc-static-range-sum.test.cpp
@@ -5,19 +5,33 @@
 
 namespace nono {
 
+template <class T>
+struct Add {
+    using value_type = T;
+    static T op(T lhs, T rhs) {
+        return lhs + rhs;
+    }
+    static T e() {
+        return T(0);
+    }
+    static T inv(T elem) {
+        return -elem;
+    }
+};
+
 void solve() {
     int n, q;
     std::cin >> n >> q;
-    FenwickTree<long long> fenwick(n);
+    FenwickTree<Add<long long>> fenwick(n);
     for (int i = 0; i < n; i++) {
         long long v;
         std::cin >> v;
-        fenwick.add(i, v);
+        fenwick.apply(i, v);
     }
     while (q--) {
         int l, r;
         std::cin >> l >> r;
-        std::cout << fenwick.sum(l, r) << '\n';
+        std::cout << fenwick.prod(l, r) << '\n';
     }
 }
 
