f3d60e69df2aa3aa271c3379bcfb5ee07a60dfe1
nono <107781334+nono-00@users.noreply.github.com>
2024-02-28 04:55:46 +0900

    add lazy-segment-tree

--- nono/data-structure/lazy-segment-tree.hpp
+++ nono/data-structure/lazy-segment-tree.hpp
@@ -1,62 +1,126 @@
 #pragma once
 
-#include <limits>
-#include <type_traits>
-
-#include <atcoder/lazysegtree>
+#include <algorithm>
+#include <bit>
+#include <cassert>
+#include <vector>
 
 namespace nono {
 
-//  todo:
-//  - 自分で書く
-//  - range add range sum などを用意する
+template <class M>
+class LazySegmentTree {
+    using T = M::value_type;
+    using F = M::func_type;
 
-template <class T>
-    requires std::is_arithmetic_v<T>
-using RangeAddRangeMinSegmentTree =
-    atcoder::lazy_segtree<T, [](const T& lhs, const T& rhs) -> T { return (lhs < rhs ? lhs : rhs); },
-                          []() -> T { return std::numeric_limits<T>::max(); }, T,
-                          [](const T& f, const T& x) -> T { return (x == std::numeric_limits<T>::max() ? x : f + x); },
-                          [](const T& lhs, const T& rhs) -> T { return lhs + rhs; },
-                          []() -> T {
-                              return static_cast<T>(0);
-                          }>;
+  public:
+    LazySegmentTree(int n): n_(n), log_(std::bit_width((unsigned)n_)), data_(2 * n_, M::e()), lazy_(n_, M::id()) {}
+    LazySegmentTree(const std::vector<T>& data)
+        : n_(data.size()),
+          log_(std::bit_width((unsigned)n_)),
+          data_(2 * n_, M::e()),
+          lazy_(n_, M::id()) {
+        std::copy(data.begin(), data.end(), data_.begin() + n_);
+        for (int i = n_ - 1; i > 0; i--) {
+            update(i);
+        }
+    }
 
-template <class T>
-    requires std::is_arithmetic_v<T>
-using RangeAddRangeMaxSegmentTree =
-    atcoder::lazy_segtree<T, [](const T& lhs, const T& rhs) -> T { return (lhs > rhs ? lhs : rhs); },
-                          []() -> T { return std::numeric_limits<T>::min(); }, T,
-                          [](const T& f, const T& x) -> T { return (x == std::numeric_limits<T>::min() ? x : f + x); },
-                          [](const T& lhs, const T& rhs) -> T { return lhs + rhs; },
-                          []() -> T {
-                              return static_cast<T>(0);
-                          }>;
+    void set(int i, T value) {
+        assert(0 <= i && i < n_);
+        i += n_;
+        for (int j = log_; j > 0; j--) {
+            if ((i >> j) << j != i) push(i >> j);
+        }
+        data_[i] = value;
+        for (int j = 1; j <= log_; j++) update(i >> j);
+    }
 
-template <class T>
-    requires std::is_arithmetic_v<T>
-using RangeSetRangeMinSegmentTree =
-    atcoder::lazy_segtree<T, [](const T& lhs, const T& rhs) -> T { return (lhs < rhs ? lhs : rhs); },
-                          []() -> T { return std::numeric_limits<T>::max(); }, T,
-                          [](const T& f, const T& x) -> T { return (f == std::numeric_limits<T>::max() ? x : f); },
-                          [](const T& lhs, const T& rhs) -> T {
-                              return (lhs == std::numeric_limits<T>::max() ? rhs : lhs);
-                          },
-                          []() -> T {
-                              return std::numeric_limits<T>::max();
-                          }>;
+    void apply(int i, F value) {
+        assert(0 <= i && i < n_);
+        i += n_;
+        for (int j = log_; j > 0; j--) {
+            if ((i >> j) << j != i) push(i >> j);
+        }
+        data_[i] = M::mapping(value, data_[i]);
+        for (int j = 1; j <= log_; j++) update(i >> j);
+    }
 
-template <class T>
-    requires std::is_arithmetic_v<T>
-using RangeSetRangeMaxSegmentTree =
-    atcoder::lazy_segtree<T, [](const T& lhs, const T& rhs) -> T { return (lhs > rhs ? lhs : rhs); },
-                          []() -> T { return std::numeric_limits<T>::min(); }, T,
-                          [](const T& f, const T& x) -> T { return (f == std::numeric_limits<T>::min() ? x : f); },
-                          [](const T& lhs, const T& rhs) -> T {
-                              return (lhs == std::numeric_limits<T>::max() ? rhs : lhs);
-                          },
-                          []() -> T {
-                              return std::numeric_limits<T>::min();
-                          }>;
+    void apply(int left, int right, F value) {
+        assert(0 <= left && left <= n_);
+        assert(left <= right && right <= n_);
+        left += n_;
+        right += n_;
+        for (int i = log_; i > 0; i--) {
+            if ((left >> i) << i != left) push(left >> i);
+            if ((right >> i) << i != right) push((right - 1) >> i);
+        }
+        int left_ = left;
+        int right_ = right;
+        for (; left < right; left >>= 1, right >>= 1) {
+            if (left & 1) eval(left++, value);
+            if (right & 1) eval(--right, value);
+        }
+        left = left_;
+        right = right_;
+        for (int i = 1; i <= log_; i++) {
+            if ((left >> i) << i != left) update(left >> i);
+            if ((right >> i) << i != right) update((right - 1) >> i);
+        }
+    }
+
+    T prod(int left, int right) {
+        assert(0 <= left && left <= n_);
+        assert(left <= right && right <= n_);
+        left += n_;
+        right += n_;
+        for (int i = log_; i > 0; i--) {
+            if ((left >> i) << i != left) push(left >> i);
+            if ((right >> i) << i != right) push((right - 1) >> i);
+        }
+        T l_result = M::e();
+        T r_result = M::e();
+        for (; left < right; left >>= 1, right >>= 1) {
+            if (left & 1) l_result = M::op(l_result, data_[left++]);
+            if (right & 1) r_result = M::op(data_[--right], r_result);
+        }
+        return M::op(l_result, r_result);
+    }
+
+    T get(int i) {
+        assert(0 <= i && i < n_);
+        i += n_;
+        for (int j = log_; j > 0; j--) {
+            if ((i >> j) << j != i) push(i >> j);
+        }
+        return data_[i];
+    }
+
+  private:
+    void update(int i) {
+        assert(0 <= i && i < n_);
+        data_[i] = M::op(data_[2 * i], data_[2 * i + 1]);
+    }
+
+    void eval(int i, F value) {
+        data_[i] = M::mapping(value, data_[i]);
+        if (i < n_) lazy_[i] = M::composition(value, lazy_[i]);
+    }
+
+    void push(int i) {
+        assert(0 <= i && i < n_);
+        if (lazy_[i] != M::id()) {
+            data_[2 * i] = M::mapping(lazy_[i], data_[2 * i]);
+            if (2 * i < n_) lazy_[2 * i] = M::composition(lazy_[i], lazy_[2 * i]);
+            data_[2 * i + 1] = M::mapping(lazy_[i], data_[2 * i + 1]);
+            if (2 * i + 1 < n_) lazy_[2 * i + 1] = M::composition(lazy_[i], lazy_[2 * i + 1]);
+            lazy_[i] = M::id();
+        }
+    }
+
+    int n_;
+    int log_;
+    std::vector<T> data_;
+    std::vector<F> lazy_;
+};
 
 }  //  namespace nono
---
+++ test/data-structure/lazy-segment-tree/lc-range_affine_range_sum.test.cpp
@@ -0,0 +1,66 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/range_affine_range_sum"
+
+#include <iostream>
+#include <utility>
+#include <vector>
+
+#include "nono/data-structure/lazy-segment-tree.hpp"
+#include "nono/math/modint.hpp"
+
+namespace nono {
+
+using Mint = nono::Modint998244353;
+
+//  c * ((a * x + b)) + d
+//
+//  c * a * x + c * b + d
+
+struct M {
+    using value_type = std::pair<Mint, int>;
+    using func_type = std::pair<Mint, Mint>;
+
+    static value_type op(value_type lhs, value_type rhs) {
+        return {lhs.first + rhs.first, lhs.second + rhs.second};
+    }
+    static value_type e() {
+        return {0, 0};
+    }
+    static value_type mapping(func_type lhs, value_type rhs) {
+        return {lhs.first * rhs.first + lhs.second * rhs.second, rhs.second};
+    }
+    static func_type composition(func_type lhs, func_type rhs) {
+        return {lhs.first * rhs.first, lhs.first * rhs.second + lhs.second};
+    }
+    static func_type id() {
+        return {1, 0};
+    }
+};
+
+void solve() {
+    int n, q;
+    std::cin >> n >> q;
+    std::vector<std::pair<Mint, int>> a(n, {0, 1});
+    for (int i = 0; i < n; i++) std::cin >> a[i].first;
+    LazySegmentTree<M> segtree(a);
+    while (q--) {
+        int t;
+        std::cin >> t;
+        if (t == 0) {
+            int l, r;
+            Mint b, c;
+            std::cin >> l >> r >> b >> c;
+            segtree.apply(l, r, std::pair<Mint, Mint>(b, c));
+        } else {
+            int l, r;
+            std::cin >> l >> r;
+            std::cout << segtree.prod(l, r).first << '\n';
+        }
+    }
+}
+
+}  //  namespace nono
+
+int main() {
+    std::cin.tie(0)->sync_with_stdio(0);
+    nono::solve();
+}
