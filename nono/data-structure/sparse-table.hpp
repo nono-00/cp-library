e0a0823b55af24a3f3e1abae807cfeea25ab3c8e
nono <107781334+nono-00@users.noreply.github.com>
2024-02-28 09:16:47 +0900

    add sparse table

---
+++ nono/data-structure/sparse-table.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <bit>
+#include <cassert>
+#include <vector>
+
+namespace nono {
+
+template <class M>
+class SparseTable {
+    using T = M::value_type;
+
+  public:
+    SparseTable(std::vector<T> data): n_(data.size()) {
+        const int log = std::bit_width((unsigned)n_);
+        data_.resize(log);
+        data_[0] = std::move(data);
+        for (int i = 1; i < log; i++) {
+            int length = 1 << i;
+            int half = length >> 1;
+            data_[i].resize(n_ - length + 1);
+            for (int j = 0; j + length <= n_; j++) {
+                data_[i][j] = M::op(data_[i - 1][j], data_[i - 1][j + half]);
+            }
+        }
+    }
+
+    T prod(int left, int right) const {
+        assert(0 <= left && left <= n_);
+        assert(left <= right && right <= n_);
+        if (left == right) return M::e();
+        const int log = 31 - std::countl_zero(static_cast<unsigned>(right - left));
+        return M::op(data_[log][left], data_[log][right - (1 << log)]);
+    }
+
+  private:
+    int n_;
+    std::vector<std::vector<T>> data_;
+};
+
+}  //  namespace nono
---
+++ test/data-structure/sparse-table/lc-staticrmq.test.cpp
@@ -0,0 +1,40 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/staticrmq"
+
+#include <iostream>
+#include <limits>
+#include <vector>
+
+#include "nono/data-structure/sparse-table.hpp"
+
+namespace nono {
+
+template <class T>
+struct Min {
+    using value_type = T;
+    static value_type op(value_type lhs, value_type rhs) {
+        return std::min(lhs, rhs);
+    }
+    static value_type e() {
+        return std::numeric_limits<T>::max();
+    }
+};
+
+void solve() {
+    int n, q;
+    std::cin >> n >> q;
+    std::vector<int> a(n);
+    for (int i = 0; i < n; i++) std::cin >> a[i];
+    SparseTable<Min<int>> sparse(a);
+    for (int i = 0; i < q; i++) {
+        int l, r;
+        std::cin >> l >> r;
+        std::cout << sparse.prod(l, r) << '\n';
+    }
+}
+
+}  //  namespace nono
+
+int main() {
+    std::cin.tie(0)->sync_with_stdio(0);
+    nono::solve();
+}
