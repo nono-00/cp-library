73d3f726e6677ccf4cf71da8d3ccd1bb927dbc9a
nono <107781334+nono-00@users.noreply.github.com>
2024-02-28 10:48:28 +0900

    add disjoint sparse table

---
+++ nono/data-structure/disjoint-sparse-table.hpp
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <cassert>
+#include <vector>
+
+namespace nono {
+
+//  - - - -*- - - -
+//  - -*- -|- -*- -
+//  -*-|-*-|-*-|-*-
+//
+//
+//  data[i][j]:
+//    000 001 | 010 011 | 100 101 | 110 111
+//
+//  prod(left, right)
+//      length = right - left
+//
+//
+
+template <class M>
+class DisjointSparseTable {
+    using T = M::value_type;
+
+  public:
+    DisjointSparseTable(const std::vector<T>& data): n_(data.size()) {
+        const int log = std::bit_width(static_cast<unsigned>(n_)) + 1;
+        data_.assign(log, std::vector<T>(n_ + 1, M::e()));
+        for (int i = 1; i < log; i++) {
+            const int length = 1 << i;
+            const int half = length >> 1;
+            for (int mid = half; mid <= n_; mid += length) {
+                const int left = mid - half;
+                const int right = std::min(mid + half - 1, n_);
+                for (int j = mid - 1; j >= left; j--) {
+                    data_[i][j] = M::op(data[j], data_[i][j + 1]);
+                }
+                for (int j = mid; j < right; j++) {
+                    data_[i][j + 1] = M::op(data_[i][j], data[j]);
+                }
+            }
+        }
+    }
+
+    T prod(int left, int right) const {
+        assert(0 <= left && left <= right);
+        assert(left <= right && right <= n_);
+        const int log = std::bit_width(static_cast<unsigned>(right ^ left));
+        return M::op(data_[log][left], data_[log][right]);
+    }
+
+  private:
+    int n_;
+    std::vector<std::vector<T>> data_;
+};
+
+}  //  namespace nono
