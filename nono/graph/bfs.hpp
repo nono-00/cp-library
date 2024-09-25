0233620a67e9e517cdec91147c02b1838ca6993c
nono <107781334+nono-00@users.noreply.github.com>
2024-09-25 11:51:23 +0900

    fix bfs

--- nono/graph/bfs.hpp
+++ nono/graph/bfs.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <optional>
 #include <queue>
 #include <vector>
 
@@ -8,26 +7,87 @@
 
 namespace nono {
 
+namespace internal {
+
+template <class T>
+class BfsResult {
+  public:
+    static constexpr T UNREACHABLE = std::numeric_limits<T>::max();
+
+    BfsResult(std::vector<T> dist, std::vector<int> parent): dist_(std::move(dist)), parent_(std::move(parent)) {}
+
+    //  source -> destの最短距離
+    T dist(int dest) const {
+        assert(0 <= dest && dest < dist_.size());
+        return dist_[dest];
+    }
+
+    //  source -> destの最短経路
+    //
+    //  dist(dest)と違って辿り着けない/距離が確定しない頂点に対して
+    //  呼び足すと空の配列を返す
+    //
+    //  source -> ... -> ... -> dest順
+    std::vector<int> path(int dest) const {
+        assert(0 <= dest && dest < dist_.size());
+        if (invalid(dest)) {
+            return {};
+        }
+        std::vector<int> result;
+        for (int pos = dest; pos != -1;) {
+            result.push_back(pos);
+            pos = parent_[pos];
+        }
+        std::reverse(result.begin(), result.end());
+        return result;
+    }
+
+    //  辿り着けないかどうか
+    bool invalid(int dest) const {
+        assert(0 <= dest && dest < dist_.size());
+        return dist_[dest] == UNREACHABLE;
+    }
+
+    std::vector<T> raw() const {
+        return dist_;
+    }
+
+  private:
+    std::vector<T> dist_;
+    //  parent_[i] = -1 => source or unrechable
+    std::vector<int> parent_;
+};
+
+}  //  namespace internal
+
 ///  brief : 幅優先探索. 使ったことがない.
 template <class T>
-std::vector<std::optional<T>> bfs(const Graph<T>& graph, int source) {
-    std::vector<std::optional<T>> dist(graph.size());
-    dist[source] = 0;
+internal::BfsResult<T> bfs(const Graph<T>& graph, const std::vector<int>& source) {
+    using Result = internal::BfsResult<T>;
+    std::vector<T> dist(graph.size(), Result::UNREACHABLE);
     std::queue<int> que;
-    que.push(source);
+    for (auto s: source) {
+        dist[s] = 0;
+        que.push(s);
+    }
 
     while (!que.empty()) {
         int u = que.front();
         que.pop();
         for (const auto& e: graph[u]) {
-            if (!dist[e.to]) {
-                dist[e.to] = *dist[u] + e.weight;
+            if (dist[e.to] == Result::UNREACHABLE) {
+                dist[e.to] = dist[u] + e.weight;
                 que.push(e.to);
             }
         }
     }
 
     return dist;
+}
+
+template <class T>
+internal::BfsResult<T> bfs(const Graph<T>& graph, int source) {
+    return bfs(graph, std::vector<int>{source});
 }
 
 }  //  namespace nono
